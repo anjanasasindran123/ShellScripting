9c7fe51d5d23f5677a400e8969e08757910977c9
Anjana S <anjanasasindran123@gmail.com>
2014-08-02 17:19:49 +0530

    java code

---
+++ Helloworld.java
@@ -0,0 +1,26 @@
+
+
+//Hello world Java//
+
+ import java.util.Scanner;
+ public class Helloworld{
+
+
+ 	public static void main(String args[]){
+                
+               
+                 Scanner in = new Scanner(System.in);
+
+                  System.out.println("Enter the value for a");
+                  int a =  in.nextInt();
+
+               
+                System.out.println("Enter the value for b");
+                int b = in.nextInt();
+                 
+                int sum=a+b;
+                 
+                 System.out.println("The Sum is" +sum);
+                
+        }
+}
